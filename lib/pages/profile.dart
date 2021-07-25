281f4339becb65236f42fd36387e3028a6ab2f8a
romero <CaptainMountain>
2021-07-25 20:49:31 +0300

    add start to display template user info

--- lib/pages/profile.dart
+++ lib/pages/profile.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 
@@ -13,26 +15,26 @@ class _ProfileState extends State<Profile> {
   static const TextStyle optionStyle =
       TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
   static const List<Widget> _widgetOptions = <Widget>[
-    Text(
-      'MAIN PAGE',
-      style: optionStyle,
-    ),
-    Text(
-      'PROFILE',
-      style: optionStyle,
-    ),
-    Text(
-      'MESSENGER',
-      style: optionStyle,
-    ),
-    Text(
-      'PEOPLES',
-      style: optionStyle,
-    ),
-    Text(
-      'MAPS',
-      style: optionStyle,
-    ),
+    // Text(
+    //   'MAIN PAGE',
+    //   style: optionStyle,
+    // ),
+    // Text(
+    //   'PROFILE',
+    //   style: optionStyle,
+    // ),
+    // Text(
+    //   'MESSENGER',
+    //   style: optionStyle,
+    // ),
+    // Text(
+    //   'PEOPLES',
+    //   style: optionStyle,
+    // ),
+    // Text(
+    //   'MAPS',
+    //   style: optionStyle,
+    // ),
   ];
 
   void _onItemTapped(int index) {
@@ -44,6 +46,9 @@ class _ProfileState extends State<Profile> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      /*
+      * APP BAR ---------------------
+      * */
       appBar: AppBar(
         title: Container(
           child: Row(
@@ -78,9 +83,251 @@ class _ProfileState extends State<Profile> {
           )
         ],
       ),
-      body: Center(
-        child: _widgetOptions.elementAt(_selectedIndex),
+
+      /*
+      * BODY --------------------------------
+      * */
+      body: SafeArea(
+        child: Container(
+          child: Column(
+            children: [
+              Padding(padding: EdgeInsets.only(top: 15.0)),
+              CircleAvatar(
+                backgroundImage: AssetImage('assets/admin.png'),
+                radius: 80.0,
+              ),
+              Padding(padding: EdgeInsets.only(top: 15.0)),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'Админ Админский',
+                    style: TextStyle(
+                        fontSize: 24.0,
+                        fontWeight: FontWeight.bold,
+                        color: Color(0xff1f282d)),
+                  ),
+                  Icon(
+                    Icons.circle,
+                    color: Colors.green,
+                    size: 10.0,
+                  )
+                ],
+              ),
+              Text(
+                'последний вход: 8 июня 2021г., 22:31',
+                style: TextStyle(fontSize: 12.0, color: Colors.black38),
+              ),
+              Padding(padding: EdgeInsets.only(top: 15.0)),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Row(
+                    children: [
+                      SizedBox(
+                        width: 150.0,
+                        child: ElevatedButton(
+                          onPressed: () {},
+                          child: Text('Сообщение'),
+                          style: ButtonStyle(
+                            backgroundColor:
+                                MaterialStateProperty.all(Color(0xff3f5561)),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Padding(padding: EdgeInsets.only(right: 10.0)),
+                      SizedBox(
+                        width: 150.0,
+                        child: ElevatedButton(
+                          onPressed: () {},
+                          child: Text('Добавить'),
+                          style: ButtonStyle(
+                            backgroundColor:
+                                MaterialStateProperty.all(Color(0xff3f5561)),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+              Padding(padding: EdgeInsets.only(top: 15.0)),
+              /*
+              * CONTAINER SUBSCRIBERS INFO
+              * */
+              Container(
+                padding: const EdgeInsets.all(15.0),
+                decoration: BoxDecoration(
+                    border: Border(top: BorderSide(color: Colors.black38))),
+                child: Container(
+                  child: 
+                    Column(
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            Column(
+                              children: [
+                                Text('Подписки: 0')
+                              ],
+                            ),
+                            Column(
+                              children: [
+                                Text('Подписчики: 0')
+                              ],
+                            )
+                          ],
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            Column(
+                              children: [
+                                Padding(padding: EdgeInsets.only(top: 15.0)),
+
+                                Text('Создано: 0')
+                              ],
+                            ),
+                            Column(
+                              children: [
+                                Padding(padding: EdgeInsets.only(top: 15.0)),
+
+                                Text('Участий: 0')
+                              ],
+                            )
+                          ],
+                        ),
+                      ],
+                    )
+                ),
+              ),
+
+              /*
+              * CONTAINER CITY AND DATE
+              * */
+              Container(
+                padding: const EdgeInsets.all(15.0),
+                decoration: BoxDecoration(
+                    border: Border(top: BorderSide(color: Colors.black38))),
+                child: Container(
+                  child: Column(
+                    children: [
+                      Row(
+                        children: [
+                          SizedBox(
+                            width: 150.0,
+                            child: Row(
+                              children: [
+                                Text('Город:'),
+                              ],
+                            ),
+                          ),
+                          Column(
+                            children: [
+                              Text('Москва'),
+                            ],
+                          ),
+                        ],
+                      ),
+                      Row(
+                        children: [
+                          Padding(padding: EdgeInsets.only(top: 25.0)),
+                          SizedBox(
+                            width: 150.0,
+                            child: Row(
+                              children: [
+                                Text('Дата рождения:'),
+                              ],
+                            ),
+                          ),
+                          Column(
+                            children: [
+                              Text('11.11.1991'),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              /*
+              * CONTAINER CITY SOCIAL MEDIA LINKS
+              * */
+              Container(
+                padding: const EdgeInsets.all(15.0),
+                decoration: BoxDecoration(
+                    border: Border(top: BorderSide(color: Colors.black38))),
+                child: Container(
+                  child: Column(
+                    children: [
+                      Row(
+                        children: [
+                          SizedBox(
+                            width: 150.0,
+                            child: Row(
+                              children: [
+                                Text('Instagram:'),
+                              ],
+                            ),
+                          ),
+                          Column(
+                            children: [
+                              Text('@captain.mountain'),
+                            ],
+                          ),
+                        ],
+                      ),
+                      Row(
+                        children: [
+                          SizedBox(
+                            width: 150.0,
+                            child: Row(
+                              children: [
+                                Text('VK:'),
+                              ],
+                            ),
+                          ),
+                          Column(
+                            children: [
+                              Text('rom_sm'),
+                            ],
+                          ),
+                        ],
+                      ),
+                      Row(
+                        children: [
+                          SizedBox(
+                            width: 150.0,
+                            child: Row(
+                              children: [
+                                Text('Facebook:'),
+                              ],
+                            ),
+                          ),
+                          Column(
+                            children: [
+                              Text('не заполнено'),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
+
+      /*
+      * BOTTOM NAVIGATION BAR --------------------------------
+      * */
       bottomNavigationBar: BottomNavigationBar(
         items: const <BottomNavigationBarItem>[
           BottomNavigationBarItem(
